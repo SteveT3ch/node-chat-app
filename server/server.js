9a23f6afa0b6d92273e6d66035b2ec365440dd84
Steve Peters <steve.t3ch@gmail.com>
2018-09-11 01:25:00 -0400

    [*] App setup and heroku config

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const { log } = console;
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+
+
+app.listen(port, () => {
+  log("Starting on port ", port);
+})
