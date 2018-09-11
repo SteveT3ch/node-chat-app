ac2f6227faa9b0fab98cedd886c7e41d1b57a332
Steve Peters <steve.t3ch@gmail.com>
2018-09-11 17:34:28 -0400

    [*] Add messaging for new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   log('New user connected');
 
+// socket.emit from Admin text Welcom to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  // socket.broadcast.emit from Admin text New user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (msg) => {
     log('createMessage', msg);
@@ -24,6 +37,12 @@ io.on('connection', (socket) => {
       text: msg.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createAt: new Date().getTime()
+    // });
   })
 
   socket.on('disconnect', () => {
