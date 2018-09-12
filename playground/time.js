f10fa0c760ddbbcfd510c28dde0775285936ebbe
Steve Peters <steve.t3ch@gmail.com>
2018-09-12 02:24:11 -0400

    [*] Format timestamp using momentjs

---
+++ playground/time.js
@@ -0,0 +1,24 @@
+// Jan 1st 1970 00:00:00 am relative to this datetime
+// milisecond
+
+// example
+// 1000 => Jan 1st 1970 00:00:01 am
+const {log} = console;
+const moment = require('moment');
+//
+// const date = moment().format('MMM do YYYY hh:mm:ss');
+// log(date);
+// how to use the createdAt timestamp
+//const createdAt = 123;
+let createdAt = moment().valueOf();
+
+log(createdAt);
+
+let now = moment(createdAt);
+log(now);
+
+//const date = moment(createdAt);
+//log(date);
+
+// const currentTime = date.format('h:mm a');
+// log(currentTime);
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,10 @@
+const moment = require('moment');
+
 let generateMessage = (from, text) => {
   return {
     from,
     text,
-    createAt: new Date().getTime()
+    createAt: moment().valueOf()
   }
 };
 
@@ -10,7 +12,7 @@ const generateLocationMessage = (from, lat, long) => {
   return {
     from,
     url: `https://www.google.com/maps?q=${lat},${long}`,
-    createdAt: new Date().getTime()
+    createdAt: moment().valueOf()
   }
 };
 
