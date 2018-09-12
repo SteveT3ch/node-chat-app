85153afbf76d8b06e78461c027443978b970deae
Steve Peters <steve.t3ch@gmail.com>
2018-09-12 00:26:27 -0400

    [*] Add Styles for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -26,14 +26,15 @@ socket.on('newLocationMessage', function (message) {
   $('#messages').append(li);
 });
 
+const messageTextBox = $('[name=message]');
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('');
   });
 });
 
@@ -43,13 +44,18 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     })
 
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
