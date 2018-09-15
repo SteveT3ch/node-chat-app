6e47dbcd5adec32db4c344976a0aeb4c269ec725
Steve Peters <steve.t3ch@gmail.com>
2018-09-14 21:20:46 -0400

    [*] Add Mustasche.js for templating

--- public/js/index.js
+++ public/js/index.js
@@ -9,23 +9,29 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
+  const template = $('#message-template').html();
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log(message);
-  var li = $('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  $('#messages').append(li);
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createAt: formattedTime
+  });
+
+  $('#messages').append(html);
+  
 });
 
 socket.on('newLocationMessage', function (message) {
   let formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log(message);
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
+  let template = $('#location-message-template').html();
+  let html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  $('#messages').append(li);
+  $('#messages').append(html);
+
 });
 
 const messageTextBox = $('[name=message]');
